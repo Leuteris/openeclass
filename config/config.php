42282de855c83839b9e3e8866c4b15573dc1f627
Leuteris <dennisritchiec@gmail.com>
2014-10-20 23:47:10 +0300

    Revert "Revert "message""
    This reverts commit 9e31dee9b0e07c178beb47ba56c727f6c13c5723.

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+/* ========================================================
+ * Open eClass 3.0 configuration file
+ * Created by install on 2014-10-12 01:14
+ * ======================================================== */
+
+$mysqlServer = 'localhost';
+$mysqlUser = 'root';
+$mysqlPassword = '';
+$mysqlMainDb = 'eclass';
