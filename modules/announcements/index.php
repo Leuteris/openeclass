9e31dee9b0e07c178beb47ba56c727f6c13c5723
Leuteris <dennisritchiec@gmail.com>
2014-10-20 23:38:54 +0300

    Revert "message"
    This reverts commit 46190285eb2a005f648fcae8a34e0e0f49e75dec.

--- modules/announcements/index.php
+++ modules/announcements/index.php
@@ -342,31 +342,6 @@ if ($is_editor) {
         else {
             $message = "<p class='success'>$langAnnAdd</p>";
         }
-        
-         //Facebook API call
-        $url = 'https://graph.facebook.com/v2.1/695730993849543/feed?   access_token=CAANapFfgn3QBAA1reXj15nCo4RgZB3cEViKnXe0i0dTDnjhirBYYjVTv46sPL6sVosAR1L832I5wvlc3ObX4JCaZA8hubsW1qgEz0sS1bpuuDQKLZCAmMEY8guSz0BiNqQwEbpiSauM0wqwtW299p8BBzJUkTVtPMaJJNSCct3baXAwY1gy';
-        $fields = array('message' => urlencode($_POST['newContent']));
-        //url-ify the da//ta for the POST
-        $fields_string = null;
-        foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
-        rtrim($fields_string, '&');
-        //open connection
-           
-        $ch = curl_init();
-        //set the url, number of POST vars, POST data
-        curl_setopt($ch,CURLOPT_URL, $url);
-        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);
-        curl_setopt($ch,CURLOPT_TIMEOUT,400);
-        curl_setopt($ch,CURLOPT_POST, count($fields));
-        curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-        //execute post
-        $result = curl_exec($ch);
-       // var_dump($result);
-       // var_dump(curl_error($ch));
-        //close connection
-        curl_close($ch);
-        
     } // end of if $submit
 
 
